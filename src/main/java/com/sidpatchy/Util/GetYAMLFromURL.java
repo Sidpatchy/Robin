4a032fa113e98955aea86d53ab0de7d53aa1fa2e
Sidpatchy <35241490+Sidpatchy@users.noreply.github.com>
2022-10-11 20:42:54 -0500

    Update GetYAMLFromURL.java

--- src/main/java/com/sidpatchy/Util/GetYAMLFromURL.java
+++ src/main/java/com/sidpatchy/Util/GetYAMLFromURL.java
@@ -7,6 +7,8 @@ import org.yaml.snakeyaml.Yaml;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +24,32 @@ public class GetYAMLFromURL {
         try {
             url = new URL(link);
             reader = new InputStreamReader(url.openStream());
+        } catch (IOException e) {
+            logger.error(e.getStackTrace());
+            logger.error("Unable to read from " + link);
+        }
+
+        return yaml.load(reader);
+    }
+
+    /**
+     * Gets YAML from URL when basic authentication is required
+     *
+     * @param username
+     * @param password
+     * @param link
+     * @return
+     */
+    public Map<String, Object> getYAMLFromURL(String username, String password, String link) {
+        URL url = null;
+        InputStreamReader reader = null;
+        try {
+            url = new URL(link);
+            URLConnection uc = url.openConnection();
+            String userpass = username + ":" + password;
+            String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
+            uc.setRequestProperty("Authorization", basicAuth);
+            reader = new InputStreamReader(uc.getInputStream());
         } catch (IOException e) {
             logger.error(e.getStackTrace());
             logger.error("Unable to read from " + link);
